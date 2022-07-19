f1ee976896a33de0e5475018230995d0f75635ab
0xngmi <0xngmi@protonmail.com>
2022-07-19 19:46:59 +0100

    return proper signature

--- src/getQuote.ts
+++ src/getQuote.ts
@@ -9,7 +9,7 @@ async function getSecret():Promise<string>{
 
 const privkey = getSecret()
 
-async function sign(price:number, deadline:number, nftContract:string){
+async function sign(price:ethers.BigNumber, deadline:number, nftContract:string){
   const signer = new ethers.Wallet(await privkey)
   const message = ethers.utils.arrayify(new ethers.utils.AbiCoder().encode([ "uint", "uint"], [ price, deadline ])+nftContract.substr(2));
   const signature = ethers.utils.splitSignature(await signer.signMessage(message))
@@ -20,15 +20,32 @@ async function sign(price:number, deadline:number, nftContract:string){
 const handler = async (
   _event: AWSLambda.APIGatewayEvent
 ): Promise<any> => {
-  const body = {}
+  const nftContract = "0x71a15Ac12ee91BF7c83D08506f3a3588143898B5"
+  const now = Math.round(Date.now()/1e3)
+  const deadline = now + 5*60; // +5 mins
+  const price = 0.1
+  const ethPrice = ethers.utils.parseEther(price.toString())
+  const signature = await sign(ethPrice, deadline, nftContract)
 
-  await sign(1, 2, "a")
+  const body = {
+    price: ethPrice.toString(),
+    deadline,
+    nftContract,
+    signature:{
+      v: signature.v,
+      r: signature.r,
+      s: signature.s
+    }
+  }
 
   return {
     statusCode: 200,
     body: JSON.stringify(body),
     headers: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Cache-Control": `max-age=${2*60}`, // 2 mins
     },
   };
 };
