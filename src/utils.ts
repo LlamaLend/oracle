01237004aa2a944843de8c3b68ee1bad981682e5
0xngmi <0xngmi@protonmail.com>
2022-09-23 20:03:58 +0100

    clean packages

--- src/utils.ts
+++ src/utils.ts
@@ -6,7 +6,3 @@ export async function getFloorNow(){
     const stats = await fetch("https://api.opensea.io/api/v1/collection/tubby-cats/stats?format=json").then(r=>r.json())
     return stats.stats.floor_price
 }
-
-export function fetchIpfsUrl(url: string) {
-    return fetch(url.replace("ipfs://", "https://cloudflare-ipfs.com/ipfs/"))
-}
