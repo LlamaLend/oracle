65115d9451aff25fa4756027ad5759a0d68b2bae
0xngmi <0xngmi@protonmail.com>
2022-10-25 17:03:26 +0100

    fix preflight

--- src/fallback.ts
+++ src/fallback.ts
@@ -1,6 +1,18 @@
 const handler = async (
-  _event: AWSLambda.APIGatewayEvent
+  event: AWSLambda.APIGatewayEvent
 ): Promise<any> => {
+  if(event.httpMethod === "OPTIONS"){
+    return {
+      statusCode: 200,
+      body: "",
+      headers: {
+        "cache-control": "max-age=3600, s-maxage=3600", // Caches preflight req on browser and proxy for 1 hour
+        "access-control-allow-methods": "OPTIONS,GET",
+        "access-control-allow-headers":
+          "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent",
+      },
+    };
+  }
   const response = {
     statusCode: 404,
     body: "This endpoint doesn't exist",
