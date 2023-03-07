d45674fd4cc1ee871f976e8165d88368a8a20e61
0xngmi <0xngmi@protonmail.com>
2023-03-07 15:42:41 +0000

    handle oldestPrice better

--- src/utils/listingFloors.ts
+++ src/utils/listingFloors.ts
@@ -64,8 +64,11 @@ async function reservoirFloor(collection: string, reservoirApiKey: string, start
             currentFloor = floors[0]
             weeklyMinimum = floors[0]
         }
+        weeklyMinimum = Math.min(weeklyMinimum, ...floors)
         const oldestPrice = changes.events[changes.events.length-1].event.previousPrice
-        weeklyMinimum = Math.min(weeklyMinimum, ...floors, oldestPrice)
+        if(oldestPrice !== null){
+          weeklyMinimum = Math.min(weeklyMinimum, oldestPrice)
+        }
         continuation = changes.continuation;
     } while(continuation !== null)
     if(currentFloor === undefined){
